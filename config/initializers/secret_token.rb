64841ffbfc54a8c5a6d87e7c001aac2ddfed5679
Will Stock <w.stock@yahoo.com>
2012-02-18 14:05:36 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apitest::Application.config.secret_token = '9c695d6503c081a0022617371540dc3885dfe0c969e7688b645da234ea7526b20972403c42ca29c79d2c454b6234995e7b4708880037d4ed52a4e153a8112935'
